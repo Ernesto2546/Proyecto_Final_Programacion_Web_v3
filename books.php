b8820e0ddc758d9f69a38d648f0e79c42edc614c
Ernesto2546 <ernestonicolas2546@gmail.com>
2024-07-20 02:29:45 -0400

    Commit inicial

---
+++ books.php
@@ -0,0 +1,8 @@
+<?php include("template/header.php") ?>
+<?php include("template/sidebar.php") ?>
+
+<main>
+    <?php include("pages/books.php") ?>
+</main>
+
+<?php include("template/footer.php") ?>
---
+++ pages/books.php
@@ -0,0 +1,3 @@
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Listado de Libros</h1>
+</div>
