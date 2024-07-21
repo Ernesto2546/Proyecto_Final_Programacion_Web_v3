00c7f0279ace94c2fd47b93d17fddcd5cb76a2bd
Ernesto2546 <ernestonicolas2546@gmail.com>
2024-07-21 16:36:56 -0400

    detalles v1

--- pages/contacts_result.php
+++ pages/contacts_result.php
@@ -6,17 +6,25 @@
             <img width="80%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThWB1II1WjPcJW59rfoony5dznkjHkwRTEGQ&s" alt="Imagen de Contactame">
         </div>
 
-        <?php if($resultado) {?>
-    
-        <div>
-            GRACIAS POR CONTACTARNOS...
+        <?php if ($resultado) { ?>
 
+            <div>
+                <div>
+                    <h3>
+                        GRACIAS POR CONTACTARNOS...
+                    </h3>
+                </div>
+                <div>
+                    <a class="btn btn-primary" href="contacts.php" role="button">Volver a comentarios</a>
+                    <a class="btn btn-primary" href="index.php" role="button">Ver Libros</a>
+                    <a class="btn btn-primary" href="authors.php" role="button">Ver Autores</a>
+                </div>
+            </div>
 
-        </div>
-
-        <?php } else {?>
+        <?php } else { ?>
             OPPS.... HUBO UN ERROR
-        <?php }?>
+
+        <?php } ?>
     </div>
 
 </div>
--- template/sidebar.php
+++ template/sidebar.php
@@ -1,18 +1,28 @@
+<?php
+    function active($currect_page){
+        $url =  explode('/', $_SERVER['REQUEST_URI']) ;
+        $current_path = end($url);  
+        
+        if($currect_page == $current_path){
+            echo 'active';
+        } 
+    }
+?>
 <div id="layoutSidenav">
     <div id="layoutSidenav_nav">
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link" href="index.php">
+                    <div class="sb-sidenav-menu-heading">Menú Principal</div>
+                    <a class="nav-link <?php active("index.php"); active(""); ?>" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                        Libros
+                        Libros 
                     </a>
-                    <a class="nav-link" href="authors.php">
+                    <a class="nav-link <?php active("authors.php")?>" href="authors.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                         Autores
                     </a>
-                    <a class="nav-link" href="contacts.php">
+                    <a class="nav-link <?php active("contacts.php")?>" href="contacts.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                         Contactos
                     </a>
